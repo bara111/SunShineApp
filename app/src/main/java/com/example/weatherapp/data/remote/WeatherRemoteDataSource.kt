1354c76912de48374e8d7ce1f334c6de442cd5c3
bara111 <am.bara059@gmail.com>
2020-02-16 10:23:32 +0200

    remove retrofit hard coded variables

--- app/src/main/java/com/example/weatherapp/data/remote/WeatherRemoteDataSource.kt
+++ app/src/main/java/com/example/weatherapp/data/remote/WeatherRemoteDataSource.kt
@@ -1,6 +1,8 @@
 package com.example.weatherapp.data.remote
 
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import com.example.weatherapp.R
 import com.example.weatherapp.data.models.ApiResponse
 import com.example.weatherapp.data.models.ErrorMessage
 import com.example.weatherapp.data.models.WeatherResponse
@@ -10,17 +12,20 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import javax.inject.Inject
 
-class WeatherRemoteDataSource @Inject constructor(private var retrofit: Retrofit) :
-    WeatherDataSource.Remote {
-    var weatherApiMutable = MutableLiveData<ApiResponse<WeatherResponse,ErrorMessage>>()
-    lateinit var weatherApi: ApiResponse<WeatherResponse,ErrorMessage>
+class WeatherRemoteDataSource @Inject constructor(
+    private var retrofit: Retrofit,
+    var context: Context
+) : WeatherDataSource.Remote {
+    var weatherApiMutable = MutableLiveData<ApiResponse<WeatherResponse, ErrorMessage>>()
+    lateinit var weatherApi: ApiResponse<WeatherResponse, ErrorMessage>
     lateinit var error: ErrorMessage
     lateinit var weatherResponse: WeatherResponse
     fun requestData() {
         val service = retrofit.create(WeatherService::class.java)
         val call = service.getCurrentWeatherData(
-            "32.22",
-            "35.22", "829740af696dc0258609d2d0a6a8472a"
+            context.resources.getString(R.string.retrofit_lat_nablus),
+            context.resources.getString(R.string.retrofit_lon_nablus),
+            context.resources.getString(R.string.retrofit_api_key)
         )
         call.enqueue(object : Callback<WeatherResponse> {
             override fun onResponse(
@@ -36,13 +41,13 @@ class WeatherRemoteDataSource @Inject constructor(private var retrofit: Retrofit
 
             override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
                 error = ErrorMessage(t.toString())
-                weatherApi= ApiResponse(null,error)
-                weatherApiMutable.value=weatherApi
+                weatherApi = ApiResponse(null, error)
+                weatherApiMutable.value = weatherApi
             }
         })
     }
 
-    override fun getResponse(): MutableLiveData<ApiResponse<WeatherResponse,ErrorMessage>> {
+    override fun getResponse(): MutableLiveData<ApiResponse<WeatherResponse, ErrorMessage>> {
         return weatherApiMutable
     }
 }
